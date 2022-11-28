04aad8a92eb68cd9c8744d3d3c4034e5839c84fa
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2022-11-28 23:17:12 +0200

    Parse .env values

--- src/db/DataSource.ts
+++ src/db/DataSource.ts
@@ -3,13 +3,13 @@ import { DataSource } from "typeorm";
 
 export const DB = new DataSource({
   type: "mysql",
-  host: '127.0.0.1',
-  port: 33060,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  synchronize: false,
-  logging: true,
+  host: String(process.env.DB_HOST).valueOf(),
+  port: Number(process.env.DB_PORT).valueOf(),
+  username: String(process.env.DB_USER).valueOf(),
+  password: String(process.env.DB_PASS).valueOf(),
+  database: String(process.env.DB_NAME).valueOf(),
+  synchronize: Boolean(Number(process.env.TYPEORM_ENTITIES_SYNC)).valueOf(),
+  logging: Boolean(Number(process.env.TYPEORM_LOGGING)).valueOf(),
   entities: [__dirname + '/../Models/*.{js,ts}'],
   subscribers: [],
   migrations: ['migrations/*.ts']
