547116c94b508b32ac94bc8c65cc3699d6c1da3c
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2024-06-06 22:23:29 +0300

    Update sanitizer

--- src/lib/Sanitizer.ts
+++ src/lib/Sanitizer.ts
@@ -1,6 +1,5 @@
 class Sanitizer {
 
-
     static sanitizeTitle(title:string):string {
         return title.replace(/(?:\r\n|\r|\n)/g, '').trim();
     }
@@ -10,14 +9,18 @@ class Sanitizer {
     }
 
     static formatDateString(dateString:string):string{
-        let date = new Date(dateString.trim());
-  
-        return [
-          date.getFullYear(),
-          date.getMonth()+1,
-          date.getDate()
-        ].join('-');
-      }    
+			let date = new Date(dateString.trim());
+
+			return [
+				date.getFullYear(),
+				date.getMonth()+1,
+				date.getDate()
+			].join('-');
+		}
+
+		static sanitizeCategory(category: string): string {
+			return category.length ? category.slice(0, 255) : category;
+		}
 }
 
 export default Sanitizer
