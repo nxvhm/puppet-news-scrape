04699c0f40e79ff06e05a910f0b28290426a45ac
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2024-06-25 20:46:21 +0200

    Update Aljazeera strategy

--- src/Strategies/Aljazeera.ts
+++ src/Strategies/Aljazeera.ts
@@ -1,44 +1,39 @@
 import CrawlingStrategy from "./CrawlingStrategy";
 
 class Aljazeera extends CrawlingStrategy {
+
     public url = 'https://www.aljazeera.com';
-    public name = 'Aljazeera News';
+    public name = 'Aljazeera';
     public pagesToCrawl = [
-        '/topics/regions/middleeast.html',
-        '/topics/regions/africa.html',
-        '/topics/regions/asia.html',
-        '/topics/regions/asia-pacific.html'
+			'/topics/regions/middleeast.html',
+			'/topics/regions/africa.html',
+			'/topics/regions/asia.html',
+			'/topics/regions/asia-pacific.html'
     ];
 
     public contentSelectors = {
-        title: 'h1 > span.headline',
-        description: '.btArticleExcerpt',
-        date: 'span.btArticleDate',
-        author: 'a.author',
-        category: '.btSubTitle',
-        text: '.btArticleBody',
+			title: 'h1 > span.headline',
+			description: '.btArticleExcerpt',
+			date: 'span.btArticleDate',
+			author: 'a.author',
+			category: '.btSubTitle',
+			text: '.btArticleBody',
     }
 
     public filterLinks(links: string[]): string[] {
-        return links.filter(link => {
+			return links.filter(link => {
+				if (link[0] == '/')
+					link = this.getUrl() + link;
 
-            if (link[0] == '/') {
-                link = this.getUrl() + link;
+				if (!this.isValidUrl(link))
+					return false;
 
-            }
-
-            if (!this.isValidUrl(link)) {
-                return false;
-            }
-
-            return link.split('/')
-                .filter(part => part.length > 0)
-                .length >= 7
-        }).map(link => {
-            return link[0] == '/'
-                ? this.getUrl()+link
-                : link;
-        })
+				return link.split('/')
+					.filter(part => part.length > 0)
+					.length >= 7
+			}).map(link => {
+				return link[0] == '/' ? this.getUrl()+link : link;
+			})
     }
 
 }
