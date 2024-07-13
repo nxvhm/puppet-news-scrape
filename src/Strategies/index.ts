b4e494f3d139cdd20651e243afa08ee09ab4b4b9
Antoan Partadzhiev <a.partadzhiev@gmail.com>
2024-07-13 19:23:16 +0200

    Guardian Strategy

--- src/Strategies/index.ts
+++ src/Strategies/index.ts
@@ -2,10 +2,12 @@ import Balkaninsight from "./Balkaninsight"
 import Aljazeera from "./Aljazeera"
 import CNN from "./CNN"
 import BBCNews from "./BBCNews"
+import Guardian from "./Guardian"
 
 export default {
 	Balkaninsight,
 	Aljazeera,
 	CNN,
-	BBCNews
+	BBCNews,
+	Guardian
 }
